32271092de0ba422589b4cf6568d1d6761d80eb2
ElseVladimir <voinvova95@gmail.com>
2020-05-27 18:56:16 +0300

    create all crud for objects

--- classes/Objects.php
+++ classes/Objects.php
@@ -27,6 +27,7 @@ class Objects
     }
 
 
+    //получает предметы
     static public function getObjects(PDO $pdo){
         try{
             $sql = 'SELECT id,title FROM objects ORDER BY id';
@@ -46,6 +47,7 @@ class Objects
         }
     }
 
+    //запись в связующую таблицу
     static public function writeObjectsToRelation(PDO $pdo, $teacherId,$objectId){
         try{
             $sql = 'INSERT INTO relation SET
@@ -57,6 +59,49 @@ class Objects
             $statement->execute();
         }catch (Exception $exception){
             echo 'Error write to relation'.$exception->getCode().$exception->getCode();
+            die();
+        }
+    }
+
+    //удаление по айди
+    static public function deleteFromObjects(PDO $pdo, $id){
+        try{
+            $sql = 'DELETE FROM objects WHERE id = :id';
+            $statement = $pdo->prepare($sql);
+            $statement->bindValue(':id',$id);
+            $statement->execute();
+        }catch (Exception $exception){
+            echo 'Error delete from objects'.$exception->getCode().$exception->getCode();
+            die();
+        }
+    }
+
+    //создает запись в таблице предметов
+    static public function writeToObjects(PDO $pdo, $title){
+        try {
+            $sql = 'INSERT INTO objects SET
+                title = :title';
+            $statement = $pdo->prepare($sql);
+            $statement->bindValue(':title', $title);
+            $statement->execute();
+        }catch (Exception $exception){
+            echo 'Error write to objects'.$exception->getCode().$exception->getCode();
+            die();
+        }
+    }
+
+    //редактирует запись в таблице предметов
+    static public function updateObject(PDO $pdo, $title, $id){
+        try{
+            $sql = 'UPDATE objects SET 
+                    title = :title
+                    WHERE id = :id';
+            $statement = $pdo->prepare($sql);
+            $statement->bindValue(':title',$title);
+            $statement->bindValue(':id',$id);
+            $statement->execute();
+        }catch (Exception $exception){
+            echo 'Error update objects'.$exception->getCode().$exception->getCode();
             die();
         }
     }
